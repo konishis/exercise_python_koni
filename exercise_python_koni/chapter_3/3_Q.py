d782ef7365a9a0366c96326d4126174dd6923576
konishis <gts.0424.koni2@gmail.com>
2021-02-02 17:58:32 +0900

    success 3_Q.py 3-9

---
+++ exercise_python_koni/chapter_3/3_Q.py
@@ -0,0 +1,54 @@
+#3-1
+# years_list = [1997,1998,1999,2000,2001,2002]
+# print(years_list)
+
+#3-2
+# years_list = [1997,1998,1999,2000,2001,2002]
+# print(years_list)
+# three_years_old = years_list[4]
+# print(three_years_old)
+
+#3-3
+# years_list = [1997,1998,1999,2000,2001,2002]
+# print(years_list)
+# year_max = years_list[-1]
+# print(year_max)
+
+#3-4
+# things = ['mozzarella','cinderella','salmonella']
+# print(things)
+
+#3-5
+# things = ['mozzarella','cinderella','salmonella']
+# print(things)
+# things[1] = things[1].capitalize()
+# print(things)
+
+#3-6
+# things = ['mozzarella','cinderella','salmonella']
+# print(things)
+# tmp = things.index('mozzarella')
+# things[tmp] = things[tmp].upper()
+# print(things)
+
+#3-7
+# things = ['mozzarella','cinderella','salmonella']
+# print(things)
+# tmp = things.index('salmonella')
+# del things[tmp]
+# print(things)
+
+#3-8
+# surprise = ['Groucho','Chico','Harpo']
+# print(surprise)
+
+#3-9
+surprise = ['Groucho','Chico','Harpo']
+print(surprise)
+surprise[-1] = surprise[-1].lower()
+print(surprise)
+surprise = surprise[::-1]
+print(surprise)
+surprise[0] = surprise[0].capitalize()
+print(surprise)
+
