56b248e1f1a8c268b77b91d257ccb7836a1713a6
konishis <gts.0424.koni2@gmail.com>
2021-02-15 11:29:40 +0900

    a

---
+++ exercise_python_koni/chapter_8/c8_1_1.py
@@ -0,0 +1,9 @@
+poem = ''' mukashi mukashi \
+    arutokoroni ojiisan to\
+    obaasan ga imasita'''
+
+print(len(poem))
+font = open('Douwa.txt', 'wt')
+font.write(poem)
+# print(poem, file=font)
+font.close()
