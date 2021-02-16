21668ca376c0807694cb8c7423f32696a17d9a9e
konishis <gts.0424.koni2@gmail.com>
2021-02-16 17:46:57 +0900

    json to xml success

--- exercise_python_koni/original/json2xml.py
+++ exercise_python_koni/original/json2xml.py
@@ -1,12 +1,16 @@
 import json
-import xmltodict
+import dicttoxml
 
 def main():
     TargetFile = open('jsonfile.json','r')
     readJsonTargetFile = json.load(TargetFile)
-    print(readJsonTargetFile)
-    transJson_to_xml = xmltodict.unparse(readJsonTargetFile)
+    # print(readJsonTargetFile)
+    transJson_to_xml = dicttoxml.dicttoxml(readJsonTargetFile)
     # print(transJson_to_xml)
     TargetFile.close()
+    TransTargetFile = open('xmlfile.xml', 'wt')
+    print(transJson_to_xml, file=TransTargetFile)
+    TransTargetFile.close()
+
 if __name__=='__main__':
     main()
