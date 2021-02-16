1cd30d9c632f4ad4082cc076528e56e54a2404e2
konishis <gts.0424.koni2@gmail.com>
2021-02-16 11:06:23 +0900

    mypc commit test

--- exercise_python_koni/original/json2xml.py
+++ exercise_python_koni/original/json2xml.py
@@ -6,7 +6,7 @@ def main():
     readJsonTargetFile = json.load(TargetFile)
     print(readJsonTargetFile)
     transJson_to_xml = xmltodict.unparse(readJsonTargetFile)
-    print(json.dumps(transJson_to_xml))
+    # print(transJson_to_xml)
     TargetFile.close()
 if __name__=='__main__':
     main()
