72fe47cb078ff2620dcc68bd2625e6becaff72c8
konishis <gts.0424.koni2@gmail.com>
2021-02-16 17:11:51 +0900

    a

--- tests/test_q1_doctest.py
+++ tests/test_q1_doctest.py
@@ -1,11 +1,30 @@
 import doctest_q1
 
+def setup(setrange):
+    global g_numrange
+    g_numrange = setrange
 
-def test_peq_1():
-    result = doctest_q1.main(3, 5, 10)
-    assert result == 23
+
+def setnums(versusnum):
+    calnumset = {num for num in range(1, g_numrange) if num % versusnum == 0}
+    return calnumset
 
 
-def test_ans():
-    result = doctest_q1.main(3, 5, 1000)
-    assert result == 233168
+def Andcollections(num1, num2):
+    return sum(setnums(num1) | setnums(num2))
+
+
+def cal(num1, num2, range):
+    """
+    >>> cal(3, 5, 10)
+    23
+    >>> doctest_q1.main(3, 5, 1000)
+    233168
+    """
+    setup(range)
+    return Andcollections(num1, num2)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
