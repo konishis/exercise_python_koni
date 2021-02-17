77dac74793f2b0b60c96cc907e4791855a08ad8f
konishis <gts.0424.koni2@gmail.com>
2021-02-17 17:43:54 +0900

    json to xml success

--- exercise_python_koni/original/json2xml.py
+++ exercise_python_koni/original/json2xml.py
@@ -1,51 +1,53 @@
 import json
 import dicttoxml
-from xml.dom import minidom
+from bs4 import BeautifulSoup
 
+"""
+○使用外部パッケージ
+    poetry add BeautifulSoup4
+    poetry add lxml
+○参考
+    jsonからxmlについて
+        https://qiita.com/kkdmgs110/items/9317057facb60764ed74
+    BeautifulSoupやパーサについて
+        https://qiita.com/PND/items/06e1053eeed69ec4f418
+        https://lets-hack.tech/programming/languages/python/beautifulsoup-xml/
+○内容
+    指定したjsonファイルを読み込み、xmlに変換する。その後パース(見やすく)し、ファイル出力する。
+    ここでは「jsonfile.json」を読み込み、「xmlfile.xml」を出力している。。
+
+"""
 
 def json2xml():
-
     translatedjsonFile = jsontodictFile(readjson())
     translatedxmlFile = dicttoxmlFile(translatedjsonFile)
-    print(translatedxmlFile)
-    writexml(translatedxmlFile)
-    parsexmlfile()
+    writexml(soupxmlfile(translatedxmlFile))
     
-    # TargetFile = open('jsonfile.json','r')
-    # readJsonTargetFile = json.load(TargetFile)
-    # print(readJsonTargetFile)
-    # transJson_to_xml = dicttoxml.dicttoxml(readJsonTargetFile)
-    # print(transJson_to_xml)
-    # TargetFile.close()
-    # TransTargetFile = open('xmlfile.xml', 'r')
-    # parsedTransTargetFile = minidom.parse(TransTargetFile)
-    # TransTargetFile.write(parsedTransTargetFile)
-    # TransTargetFile.close()
     
 def readjson():
     TargetFile = open('jsonfile.json','r')
     return TargetFile
 
+
 def writexml(writeFile):
-    with open('xmlfile.xml', 'wb') as xmlFile:
+    with open('xmlfile.xml', 'wt') as xmlFile:
         xmlFile.write(writeFile)
 
-def parsexmlfile():
-    # with open('xmlfile.xml', 'wb') as parsexmlFile:
-    parsedTransTargetFile = minidom.parse('xmlfile.xml')
-    with open('xmlfile.xml', 'wt') as xmlFile:
-        xmlFile.write(parsedTransTargetFile.toxml())
-    # writexml(parsedTransTargetFile.toxml())
-    # writexml(parsedTransTargetFile)
+
+def soupxmlfile(soupfile):
+    soup = BeautifulSoup(soupfile, "lxml")
+    return soup.prettify()
 
 
 def jsontodictFile(Targetfile):
     translateddictFile = json.load(Targetfile)
     return translateddictFile
 
+
 def dicttoxmlFile(Targetfile):
     translatedxmlFile = dicttoxml.dicttoxml(Targetfile)
     return translatedxmlFile
+
 
 if __name__=='__main__':
     json2xml()
